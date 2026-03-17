9d943eb985c5fde441918790ba8ee0d379765d52
Nico Noell <nico.noell@tum.de>
2026-03-17 16:05:10 +0100

    Minor Changes to TicTacToe game

--- tictactoe/src/board/cell.rs
+++ tictactoe/src/board/cell.rs
@@ -3,7 +3,7 @@ use super::player::Player;
 use core::fmt;
 
 #[derive(Clone, Copy, PartialEq)]
-pub(super) struct Cell (pub Option<Player>);
+pub struct Cell (pub Option<super::Player>);
 
 impl fmt::Display for Cell {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -15,15 +15,15 @@ impl fmt::Display for Cell {
 }
 
 impl Cell {
-    pub(super) fn is_empty(&self) -> bool {
+    pub fn is_empty(&self) -> bool {
         self.0.is_none()
     }
 
-    pub(super) fn put(&mut self, player: Player) {
+    pub fn put(&mut self, player: Player) {
         self.0 = Some(player);
     }
 
-    pub(super) fn get(&self) -> Option<Player> {
+    pub fn get(&self) -> Option<Player> {
         self.0
     }
 }
